50ac8eda82b026bc266807a0fbfd5a49b7d8f582
Jaehyeon Park <skystar@skystar.dev>
2021-10-09 14:44:50 +0900

    handle error and upload completion in upload ui

--- webapp/src/upload.rs
+++ webapp/src/upload.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use aead::generic_array::GenericArray;
 use chacha20poly1305::aead::{Aead, NewAead};
 use chacha20poly1305::{Key, XChaCha20Poly1305};
@@ -20,10 +22,12 @@ pub enum UploadMsg {
     PassphraseInput,
     UploadStart,
     Progress(ProgressInfo),
+    UploadError(UploadError),
+    UploadComplete(i64),
 }
 
 #[derive(Debug)]
-enum MyError {
+pub enum UploadError {
     JsValue(JsValue),
     Aead(aead::Error),
     Remote(String),
@@ -35,11 +39,14 @@ pub enum ProgressInfo {
 
 pub struct UploadComponent {
     link: ComponentLink<Self>,
+    base_uri: Option<String>,
     selected_file: Option<web_sys::File>,
     passphrase_ref: NodeRef,
     passphrase_available: bool,
     file_size: Option<usize>,
     uploaded_size: Option<usize>,
+    file_id: Option<i64>,
+    upload_error: Option<UploadError>,
 }
 
 fn file_input(comp: &UploadComponent) -> Html {
@@ -65,18 +72,34 @@ fn file_input(comp: &UploadComponent) -> Html {
     }
 }
 
+fn join_uri(base_uri: &str, file_id: i64) -> String {
+    if base_uri.ends_with('/') {
+        format!("{}{}", base_uri, file_id)
+    } else {
+        format!("{}/{}", base_uri, file_id)
+    }
+}
+
 impl Component for UploadComponent {
     type Message = UploadMsg;
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let base_uri = match yew::utils::document().base_uri() {
+            Ok(uri) => uri,
+            Err(_) => None,
+        };
+
         Self {
             link,
+            base_uri,
             selected_file: None,
             passphrase_ref: NodeRef::default(),
             passphrase_available: false,
             file_size: None,
             uploaded_size: None,
+            file_id: None,
+            upload_error: None,
         }
     }
 
@@ -84,6 +107,8 @@ impl Component for UploadComponent {
         match msg {
             UploadMsg::FileChanged(file) => {
                 let file_size = file.size() as usize;
+                self.file_id = None;
+                self.uploaded_size = None;
                 self.file_size = Some(file_size);
                 self.selected_file = Some(file);
                 self.passphrase_available = false;
@@ -100,6 +125,9 @@ impl Component for UploadComponent {
                 true
             }
             UploadMsg::UploadStart => {
+                self.upload_error = None;
+                self.file_id = None;
+                self.uploaded_size = None;
                 if !self.passphrase_available {
                     return false;
                 }
@@ -178,9 +206,10 @@ impl Component for UploadComponent {
                 // read file
                 let stream = wasm_streams::ReadableStream::from_raw(sys_stream).into_stream();
 
+                // stream which read files and transforms that `Uint8Array`s to `Result<Vec<u8>>`.
                 let fut = stream
                     .and_then(|b| async move { b.dyn_into::<Uint8Array>() })
-                    .map_err(MyError::JsValue)
+                    .map_err(UploadError::JsValue)
                     .map_ok(|arr| arr.to_vec());
 
                 let mut fut = Box::pin(fut);
@@ -188,17 +217,20 @@ impl Component for UploadComponent {
                 let stream_nonce = *stream_nonce;
                 let filename_nonce = *filename_nonce;
                 let clink = self.link.clone();
-                let encrypt = async move {
+
+                // core logic of streaming upload / encryption
+                let encrypt_routine = async move {
+                    // use stream encryptor
                     let mut encryptor =
                         aead::stream::EncryptorBE32::from_aead(cipher, &stream_nonce);
-                    // send starting request
+                    // send prepare request
                     let client = reqwest::Client::new();
                     let form = Form::new()
                         .part("stream_nonce", Part::stream(stream_nonce.to_vec()))
                         .part("filename_nonce", Part::stream(filename_nonce.to_vec()))
                         .part("salt", Part::stream(salt.to_vec()))
                         .part("filename", Part::stream(encrypted_filename));
-                    let id = match client
+                    let file_id = match client
                         .post(build_url("/prepare_upload"))
                         .multipart(form)
                         .send()
@@ -206,7 +238,7 @@ impl Component for UploadComponent {
                     {
                         Ok(resp) => {
                             if resp.status() != 200 {
-                                return Err(MyError::Remote(format!(
+                                return Err(UploadError::Remote(format!(
                                     "prepare_upload status != 200, but {}",
                                     resp.status()
                                 )));
@@ -215,7 +247,7 @@ impl Component for UploadComponent {
                                 match resp.bytes().await {
                                     Ok(b) => b.to_vec(),
                                     Err(_) => {
-                                        return Err(MyError::Remote(
+                                        return Err(UploadError::Remote(
                                             "failed to read resp body".into(),
                                         ));
                                     }
@@ -226,13 +258,13 @@ impl Component for UploadComponent {
                                     if let Some(v) = v.get("id").and_then(Value::as_i64) {
                                         v
                                     } else {
-                                        return Err(MyError::Remote(
+                                        return Err(UploadError::Remote(
                                             "failed to deserialize body".into(),
                                         ));
                                     }
                                 }
                                 Err(_) => {
-                                    return Err(MyError::Remote(
+                                    return Err(UploadError::Remote(
                                         "failed to deserialize body".into(),
                                     ));
                                 }
@@ -240,15 +272,19 @@ impl Component for UploadComponent {
                         }
                         Err(e) => {
                             log::error!("remote error: {:?}", e);
-                            return Err(MyError::Remote("failed to request prepare_upload".into()));
+                            return Err(UploadError::Remote(
+                                "failed to request prepare_upload".into(),
+                            ));
                         }
                     };
 
-                    let id = id.to_be_bytes();
+                    let id = file_id.to_be_bytes();
                     let mut seq: i64 = 1;
                     let mut buffer = Vec::<u8>::with_capacity(BLOCK_SIZE);
+                    // start encryption and upload
                     while let Some(v) = fut.try_next().await? {
                         let mut v: &[u8] = v.as_ref();
+                        // divide inputs into fixed block size
                         while buffer.len() + v.len() >= BLOCK_SIZE {
                             let split_idx = BLOCK_SIZE - buffer.len();
                             buffer.extend(&v[..split_idx]);
@@ -257,10 +293,11 @@ impl Component for UploadComponent {
                             // maybe there is more good way to handle this
                             let chunk = encryptor
                                 .encrypt_next(buffer.as_ref())
-                                .map_err(MyError::Aead)?;
+                                .map_err(UploadError::Aead)?;
                             let chunk_len = chunk.len();
                             let id = id.to_vec();
                             let seq_b = seq.to_be_bytes().to_vec();
+                            // upload encrypted chunk to server
                             let form = Form::new()
                                 .part("id", Part::bytes(id))
                                 .part("seq", Part::bytes(seq_b))
@@ -274,14 +311,16 @@ impl Component for UploadComponent {
                             {
                                 Ok(resp) => {
                                     if resp.status() != 200 {
-                                        return Err(MyError::Remote(format!(
+                                        return Err(UploadError::Remote(format!(
                                             "upload status != 200, but {}",
                                             resp.status()
                                         )));
                                     }
                                 }
                                 Err(_) => {
-                                    return Err(MyError::Remote("failed to upload chunk".into()));
+                                    return Err(UploadError::Remote(
+                                        "failed to upload chunk".into(),
+                                    ));
                                 }
                             }
                             buffer.clear();
@@ -297,13 +336,13 @@ impl Component for UploadComponent {
                     // upload last chunk
                     let chunk = encryptor
                         .encrypt_last(buffer.as_ref())
-                        .map_err(MyError::Aead)?;
-                    let id = id.to_vec();
-                    let seq_b = seq.to_be_bytes().to_vec();
+                        .map_err(UploadError::Aead)?;
+                    let id_b = id.to_vec();
+                    let seq = seq.to_be_bytes().to_vec();
                     let chunk_len = chunk.len();
                     let form = Form::new()
-                        .part("id", Part::bytes(id))
-                        .part("seq", Part::bytes(seq_b))
+                        .part("id", Part::bytes(id_b))
+                        .part("seq", Part::bytes(seq))
                         .part("is_last", Part::bytes(vec![1]))
                         .part("content", Part::stream(chunk));
                     match client
@@ -314,27 +353,32 @@ impl Component for UploadComponent {
                     {
                         Ok(resp) => {
                             if resp.status() != 200 {
-                                return Err(MyError::Remote(format!(
+                                return Err(UploadError::Remote(format!(
                                     "upload status != 200, but {}",
                                     resp.status()
                                 )));
                             }
                         }
                         Err(_) => {
-                            return Err(MyError::Remote("failed to upload chunk".into()));
+                            return Err(UploadError::Remote("failed to upload chunk".into()));
                         }
                     }
                     clink.send_message(UploadMsg::Progress(ProgressInfo::UploadBytes(chunk_len)));
+                    clink.send_message(UploadMsg::UploadComplete(file_id));
 
                     Ok(())
                 };
-                spawn_local(encrypt.map(|r: Result<(), MyError>| {
+
+                let clink = self.link.clone();
+                // spawn entire routine in promise
+                // TODO: research Web Workers and try to gain more performance
+                spawn_local(encrypt_routine.map(move |r: Result<(), UploadError>| {
                     if let Err(e) = r {
                         log::error!("encryption error: {:?}", e);
+                        clink.send_message(UploadMsg::UploadError(e));
                     }
                 }));
 
-                // TODO: show status: loading file
                 true
             }
             UploadMsg::Progress(info) => {
@@ -350,6 +394,16 @@ impl Component for UploadComponent {
                         self.uploaded_size = Some(after);
                     }
                 }
+
+                true
+            }
+            UploadMsg::UploadError(err) => {
+                self.upload_error = Some(err);
+
+                true
+            }
+            UploadMsg::UploadComplete(file_id) => {
+                self.file_id = Some(file_id);
 
                 true
             }
@@ -396,6 +450,48 @@ impl Component for UploadComponent {
             ((uploaded as f64 / file_size as f64) * (100_f64)) as usize
         };
 
+        let mut file_uri_class = vec!["flex", "justify-center", "mb-4"];
+        if self.file_id.is_none() || self.upload_error.is_some() {
+            file_uri_class.push("hidden");
+        }
+        let file_uri_component = match &self.base_uri {
+            Some(base_uri) => {
+                html! {
+                    <div class=classes!(file_uri_class)>
+                        <span class=classes!("mr-2")>{ "Your file: " }</span>
+                        <a class=classes!("text-blue-400") target="_blank" href={join_uri(base_uri, self.file_id.unwrap_or(0))}>
+                            { join_uri(base_uri, self.file_id.unwrap_or(0)) }
+                        </a>
+                    </div>
+                }
+            }
+            None => {
+                html! {
+                    <div class=classes!(file_uri_class)>
+                        <span>{ format!("Your file id: {}", self.file_id.unwrap_or(0)) }</span>
+                    </div>
+                }
+            }
+        };
+
+        let mut upload_error_class = vec!["flex", "justify-center", "mb-4"];
+        if self.upload_error.is_none() {
+            upload_error_class.push("hidden");
+        }
+        let upload_error_text = match &self.upload_error {
+            Some(err) => match err {
+                UploadError::JsValue(_) => Cow::from("File read error"),
+                UploadError::Aead(_) => Cow::from("Encryption error"),
+                UploadError::Remote(msg) => Cow::from(format!("Server error: {}", msg)),
+            },
+            None => Cow::from(""),
+        };
+        let upload_error_component = html! {
+            <div class=classes!(upload_error_class)>
+                <span class=classes!("text-red-300")>{ upload_error_text }</span>
+            </div>
+        };
+
         html! {
             <>
                 { file_input(self) }
@@ -431,6 +527,8 @@ impl Component for UploadComponent {
                         { "UPLOAD" }
                     </button>
                 </div>
+                { upload_error_component }
+                { file_uri_component }
             </>
         }
     }
