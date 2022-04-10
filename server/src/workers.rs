cffa28d1f8666f6181ab5429d9574679c682f736
Jaehyeon Park <skystar@skystar.dev>
2022-04-11 02:32:09 +0900

    fix worker delete query to work with sqlite

--- server/src/workers.rs
+++ server/src/workers.rs
@@ -29,7 +29,7 @@ pub async fn delete_expired(state: Arc<State>, config: Config) {
         {
             // prepare statement
             let query =
-                "delete from files where (now() > created_at + ?1 * interval '1' second) returning id";
+                "delete from files where (current_timestamp > created_at + ?1) returning id";
             let mut stmt = match tx.prepare(query) {
                 Ok(stmt) => stmt,
                 Err(err) => {
