480eacf10eb4576482fc95d9b074fb3769b3b4f7
Jaehyeon Park <skystar@skystar.dev>
2022-04-11 11:53:33 +0900

    always generate asset.css if not exists

--- webapp/pre_build.sh
+++ webapp/pre_build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+if [ ! -f "./asset.css" ]; then
+    tailwindcss -o ./asset.css
+fi
 if [ "$TRUNK_PROFILE" = "release" ]; then
     cp ./asset.css ./asset.css.bak
     NODE_ENV=production tailwindcss -c ./tailwind.config.js -o ./asset.css --minify
-elif [ "$TRUNK_PROFILE" = "debug" ]; then
-    if [ ! -f "./asset.css" ]; then
-        tailwindcss -o ./asset.css
-    fi
 fi
